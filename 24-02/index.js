208f7884258705b173471b89013b986c4ab10aeb
Asadbek <asadbek010azamjonov@gmail.com>
2025-02-24 11:04:21 +0500

    takrorlash

--- 24-02/index.js
+++ 24-02/index.js
@@ -1,1 +1,20 @@
 console.log('Welcome Message');
+// isAllowed(19);
+
+// let isAllowed = function (param) {
+// 	if (param >= 18) {
+// 		console.log('Allowed');
+// 	} else {
+// 		console.log('not Allowed');
+// 	}
+// };
+
+// Function expression
+
+// =>
+
+let functionExpres = () => {
+	console.log('hey');
+};
+
+functionExpres();
