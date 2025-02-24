e2382c86ddee6ff7b6d0f8e135adc3057f7f5b4f
Asadbek <asadbek010azamjonov@gmail.com>
2025-02-24 11:45:00 +0500

    yangi dars va vazifalar

--- 24-02/index.js
+++ 24-02/index.js
@@ -1,3 +1,9 @@
+function logTitle(param) {
+	console.log(
+		`------------------------------------------------${param}-------------------------------------------------`
+	);
+}
+
 console.log('Welcome Message');
 // isAllowed(19);
 
@@ -18,3 +24,53 @@ let functionExpres = () => {
 };
 
 functionExpres();
+logTitle('24-02-yangi dars');
+
+const fruits = ['olma', 'apple', 'banan'];
+// fruits.forEach((item, index) => console.log(index));
+
+const arr = [
+	{
+		name: 'Asadbek',
+		age: '16',
+	},
+	{
+		name: 'Duryodbek',
+		age: '24',
+		ism: true,
+	},
+	{
+		name: 'Abdurauf',
+		age: '18',
+	},
+];
+
+console.log(arr[2].age);
+// arr.forEach((item, index) => console.log(item.ism));
+
+function isAllowed(param) {
+	arr.forEach((item, index) => {
+		if (item.age >= 18) {
+			console.log(`Foydalanuvchi telefondan foydalana oladi: ${item.name}`);
+		} else {
+			console.log(`yosh yetmaydi: ${item.name}`);
+		}
+	});
+}
+
+isAllowed(arr);
+
+// Keywords:
+
+// function turlari
+// ternary turlari
+// iteratsiya haqida
+// .forEach() -> iteratsiya
+
+logTitle('Vazifalar');
+
+// 1) bitta funksiya ozida arr sifatida param kutsin va uni logga olsin
+
+// 2) tepadagi funksiyaga kiritilagan arrayni index bo'ylab olsin.
+
+// 3) yangi funksiya kk o'zida array of objectlarni param sifatida kutsin va har bir objectni arraydan chiqarsin
