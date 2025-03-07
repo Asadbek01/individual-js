6672cb2dafa429ef659e841310b9dbb3f6acc8dc
Asadbek <asadbek010azamjonov@gmail.com>
2025-03-07 11:39:20 +0500

    07-03 dars

---
+++ 07-03/index.js
@@ -0,0 +1,21 @@
+const newText = 'New Text';
+
+const idBilan = document.getElementById('h1-tag'); // ID bilan olish
+const backButton = document.getElementById('back-button');
+const classBilan = document.getElementsByClassName('h1-tag');
+console.log(document.getElementsByTagName('h1'));
+
+const queryBilan = document.querySelector('h1'); // Selector bilan olish
+console.log(queryBilan);
+let originalText = idBilan.innerText;
+console.log(originalText);
+
+idBilan.addEventListener('click', e => {
+	e.target.innerText = newText;
+});
+
+backButton.addEventListener('click', e => {
+	idBilan.innerText = originalText;
+});
+
+const h1 = document.getElementById('h1-tag');
