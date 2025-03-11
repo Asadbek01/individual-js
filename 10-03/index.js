a723bd55ec1c762a202ec09daa9f814286e18e4e
Asadbek <asadbek010azamjonov@gmail.com>
2025-03-12 00:59:18 +0500

    challenged task

--- 10-03/index.js
+++ 10-03/index.js
@@ -52,3 +52,16 @@ submitBtn.addEventListener('click', () => {
 });
 
 // DOM -> getElementById, querySelector,
+
+// Vazifalar:
+
+// 1. HTML sahifada 'user-info' ID li div, 'users' class li ro'yxat (ul), va 5 ta <p> teglari mavjud.
+// 2. JavaScript yordamida:
+//    - 'user-info' ID li elementni toping
+//    - 'users' class li barcha elementlarni toping
+//    - Barcha <p> teglarini toping
+// 3. Natijalarni console.log() orqali ko'rsating
+// 4. Har bir usul (getElementById, querySelector, querySelectorAll) ning farqlarini tushuntiring
+
+// 5. inputlarga qiymat kiritilganda, submit tugmasi bosilganda, inputlardagi qiymatlarni consolga chiqaring
+// 6. Agar inputlarga qiymat kiritilmagan bo'lsa, consolga "Iltimos, inputlarni to'ldiring" degan xabarni chiqaring
